53b12431d01ea602b9d85c41b54427f721afd704
Alex Keizer <alex@keizer.dev>
2023-10-18 21:28:24 +0100

    Add mathlib

--- lakefile.lean
+++ lakefile.lean
@@ -9,7 +9,10 @@ lean_lib LeanMeetup {
   -- add library configuration options here
 }
 
-@[defaultTarget]
+require mathlib from git
+  "https://github.com/leanprover-community/mathlib4.git"
+
+@[default_target]
 lean_exe leanMeetup {
   root := `Main
 }

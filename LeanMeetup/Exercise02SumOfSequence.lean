655514f186b4ac4e78cb67ad242d62453cff166c
Alex Keizer <alex@keizer.dev>
2023-10-26 17:14:30 +0100

    Add setup as exercise file

---
+++ LeanMeetup/Exercise02SumOfSequence.lean
@@ -0,0 +1,20 @@
+import Mathlib
+
+/--
+  `sumOfSequence n` is the sum of the sequence `0, 1, ..., n`
+-/
+def sumOfSequence : Nat → Nat
+  | 0   => 0
+  | n+1 => sumOfSequence n + (n+1)
+
+#eval sumOfSequence 0
+#eval sumOfSequence 1
+#eval sumOfSequence 2
+#eval sumOfSequence 3
+#eval sumOfSequence 4
+#eval sumOfSequence 5
+#eval sumOfSequence 6
+
+theorem sumOfSequence_eq :
+    sumOfSequence n = n * (n + 1) / 2 := by
+  sorry
